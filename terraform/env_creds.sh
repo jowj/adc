f83ec22eb8623c18512ea5565c050482ba634e20
josiah <me@jowj.net>
2023-08-06 20:41:36 -0500

    Move to using DO:Spaces as state backend.
    - Update provider.tf to include details about the backend
    - Include AWS PROFILE export in env creds
    - Update readme.

--- terraform/env_creds.sh
+++ terraform/env_creds.sh
@@ -3,3 +3,4 @@
 export TF_VAR_PM_API_TOKEN_ID=$(pass pm_api_token_id)
 export TF_VAR_PM_API_TOKEN_SECRET=$(pass pm_api_token_secret)
 export TF_VAR_DO_PAT=$(pass do_pat)
+export AWS_PROFILE=digitalocean
