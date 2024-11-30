e62ed700ca7bfe293b99f6b785bb11f998a2360c
josiah <me@jowj.net>
2024-11-30 09:55:20 -0600

    Remove unneeded awful DNS entries.

--- terraform/do_domains/records_awful.club.tf
+++ terraform/do_domains/records_awful.club.tf
@@ -1,112 +1,3 @@
-
-resource "digitalocean_record" "tfer--103887854" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "nova"
-  port     = "0"
-  priority = "0"
-  ttl      = "900"
-  type     = "A"
-  value    = "167.172.201.83"
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--108277404" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "element"
-  port     = "0"
-  priority = "0"
-  ttl      = "900"
-  type     = "CNAME"
-  value    = "chat.awful.club."
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--116320524" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "rss"
-  port     = "0"
-  priority = "0"
-  ttl      = "3600"
-  type     = "A"
-  value    = "134.209.53.112"
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--152768913" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "dimension"
-  port     = "0"
-  priority = "0"
-  ttl      = "900"
-  type     = "CNAME"
-  value    = "matrix.awful.club."
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--155501426" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "stats"
-  port     = "0"
-  priority = "0"
-  ttl      = "900"
-  type     = "CNAME"
-  value    = "matrix.awful.club."
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--353715597" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "halo"
-  port     = "0"
-  priority = "0"
-  ttl      = "900"
-  type     = "A"
-  value    = "138.68.15.125"
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--354353835" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "monitor"
-  port     = "0"
-  priority = "0"
-  ttl      = "900"
-  type     = "A"
-  value    = "100.72.101.61"
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--355653914" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "requests"
-  port     = "0"
-  priority = "0"
-  ttl      = "900"
-  type     = "A"
-  value    = "100.72.101.61"
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--361691456" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "drone"
-  port     = "0"
-  priority = "0"
-  ttl      = "900"
-  type     = "A"
-  value    = "100.108.51.49"
-  weight   = "0"
-}
-
 resource "digitalocean_record" "tfer--50599690" {
   domain   = "awful.club"
   flags    = "0"
@@ -155,41 +46,6 @@ resource "digitalocean_record" "tfer--50599693" {
   weight   = "0"
 }
 
-resource "digitalocean_record" "tfer--50601774" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "slack-bot"
-  port     = "0"
-  priority = "0"
-  ttl      = "3600"
-  type     = "A"
-  value    = "134.209.53.112"
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--50892537" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "bouncer"
-  port     = "0"
-  priority = "0"
-  ttl      = "3600"
-  type     = "A"
-  value    = "134.209.53.112"
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--53060224" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "my"
-  port     = "0"
-  priority = "0"
-  ttl      = "3600"
-  type     = "A"
-  value    = "142.93.118.115"
-  weight   = "0"
-}
 
 resource "digitalocean_record" "tfer--53273061" {
   domain   = "awful.club"
@@ -215,29 +71,7 @@ resource "digitalocean_record" "tfer--53273081" {
   weight   = "0"
 }
 
-resource "digitalocean_record" "tfer--56070115" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "monitoring"
-  port     = "0"
-  priority = "0"
-  ttl      = "3600"
-  type     = "A"
-  value    = "134.209.53.112"
-  weight   = "0"
-}
 
-resource "digitalocean_record" "tfer--58893317" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "cloud"
-  port     = "0"
-  priority = "0"
-  ttl      = "3600"
-  type     = "A"
-  value    = "134.209.53.112"
-  weight   = "0"
-}
 
 resource "digitalocean_record" "tfer--61254814" {
   domain   = "awful.club"
@@ -251,53 +85,6 @@ resource "digitalocean_record" "tfer--61254814" {
   weight   = "0"
 }
 
-resource "digitalocean_record" "tfer--61255364" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "_matrix._tcp"
-  port     = "8448"
-  priority = "10"
-  ttl      = "900"
-  type     = "SRV"
-  value    = "matrix.awful.club."
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--61255373" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "_matrix-identity._tcp"
-  port     = "443"
-  priority = "10"
-  ttl      = "900"
-  type     = "SRV"
-  value    = "matrix.awful.club."
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--61255385" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "riot"
-  port     = "0"
-  priority = "0"
-  ttl      = "43200"
-  type     = "CNAME"
-  value    = "chat.awful.club."
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--61255389" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "matrix"
-  port     = "0"
-  priority = "0"
-  ttl      = "900"
-  type     = "CNAME"
-  value    = "chat.awful.club."
-  weight   = "0"
-}
 
 resource "digitalocean_record" "tfer--68245405" {
   domain   = "awful.club"
@@ -335,42 +122,6 @@ resource "digitalocean_record" "tfer--74148930" {
   weight   = "0"
 }
 
-resource "digitalocean_record" "tfer--81354418" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "vpn"
-  port     = "0"
-  priority = "0"
-  ttl      = "900"
-  type     = "A"
-  value    = "134.209.53.112"
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--89005178" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "discarch"
-  port     = "0"
-  priority = "0"
-  ttl      = "900"
-  type     = "A"
-  value    = "159.65.66.143"
-  weight   = "0"
-}
-
-resource "digitalocean_record" "tfer--95970598" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "ocean"
-  port     = "0"
-  priority = "0"
-  ttl      = "900"
-  type     = "NS"
-  value    = "ns1.awful.club."
-  weight   = "0"
-}
-
 resource "digitalocean_record" "tfer--95970600" {
   domain   = "awful.club"
   flags    = "0"
@@ -395,19 +146,8 @@ resource "digitalocean_record" "tfer--95970760" {
   weight   = "0"
 }
 
-resource "digitalocean_record" "tfer--98495666" {
-  domain   = "awful.club"
-  flags    = "0"
-  name     = "whoami"
-  port     = "0"
-  priority = "0"
-  ttl      = "900"
-  type     = "A"
-  value    = "134.209.53.112"
-  weight   = "0"
-}
 
-resource "digitalocean_record" mattermost-dns {
+resource "digitalocean_record" "mattermost-dns" {
   domain   = "awful.club"
   flags    = "0"
   name     = "mattermost"
