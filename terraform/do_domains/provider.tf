080b3c53ba0512682289366c3c7802748df0ae91
josiah <me@jowj.net>
2024-12-06 17:39:41 -0600

    Lint provider.tf

--- terraform/do_domains/provider.tf
+++ terraform/do_domains/provider.tf
@@ -1,27 +1,27 @@
 terraform {
-    required_providers {
-	digitalocean = {
-	    source = "digitalocean/digitalocean"
-	    version = "~> 2.29.0"
-	}
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.29.0"
     }
-    backend "s3" {
-	key      = "domains/terraform.tfstate"
-	bucket   = "deploy-state"
-	region   = "us-west-2"
-	endpoint = "https://sfo2.digitaloceanspaces.com"
-	skip_region_validation      = true
-	skip_credentials_validation = true
-	skip_metadata_api_check     = true
+  }
+  backend "s3" {
+    key                         = "domains/terraform.tfstate"
+    bucket                      = "deploy-state"
+    region                      = "us-west-2"
+    endpoint                    = "https://sfo2.digitaloceanspaces.com"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
 
-	# This is actually not needed, but declaring it here helps me remember where its supposed to live.	
-	#shared_credentials_files = ["~/.aws/credentials"]
-	#shared_config_files = ["~/.aws/config"]    	
-    }
-    
+    # This is actually not needed, but declaring it here helps me remember where its supposed to live.
+    #shared_credentials_files = ["~/.aws/credentials"]
+    #shared_config_files = ["~/.aws/config"]
+  }
+
 }
 
 
 provider "digitalocean" {
-    token = var.DO_PAT
+  token = var.DO_PAT
 }
